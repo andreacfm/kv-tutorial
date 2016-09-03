b1dafd3929fdb82ec614afdd91daf18130860ebe
Andrea Campolonghi <acampolonghi@gmail.com>
2016-09-03 16:01:35 +0200

    Process monitoring

---
+++ lib/kv/registry.ex
@@ -0,0 +1,50 @@
+defmodule KV.Registry do
+  use GenServer
+
+  ## Client API
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  def create(server, name) do
+    GenServer.call(server, {:create, name})
+  end
+
+  ## GenServer Callbacks
+  def init(:ok) do
+    names = %{}
+    refs = %{}
+    {:ok, {names, refs}}
+  end
+
+  def handle_call({:lookup, name}, _from ,{names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
+  end
+
+  def handle_call({:create, name}, _from, {names, refs}) do
+    if Map.has_key?(names, name) do
+      {:noreply, {names, refs}}
+    else
+      {:ok, bucket} = KV.Bucket.start_link
+      ref = Process.monitor(bucket)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, bucket)
+      {:reply, {names, refs}, {names, refs}}
+    end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+
+end
